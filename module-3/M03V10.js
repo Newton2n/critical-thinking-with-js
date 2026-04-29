c9bc4b32f15cc70423e5bddccd08dbe88321b1dc
Newton <156936866+Newton2n@users.noreply.github.com>
2026-04-29 17:10:55 +0600

    chore:moudle-3 linkded list remove method implement

---
+++ module-3/M03V10.js
@@ -0,0 +1,144 @@
+class Node {
+  constructor(value) {
+    this.data = value;
+    this.next = null;
+  }
+}
+
+class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+  append(value) {
+    const newNode = new Node(value);
+
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      this.tail.next = newNode;
+      this.tail = newNode;
+    }
+    this.length++;
+  }
+
+  prepend(value) {
+    const newNode = new Node(value);
+
+    if (this.length === 0) {
+      this.head = newNode;
+      this.tail = newNode;
+    } else {
+      newNode.next = this.head;
+      this.head = newNode;
+    }
+    this.length++;
+  }
+
+  insert(index, value) {
+    console.log(this.length, index);
+    if (index < 0 || index > this.length) {
+      throw new Error("Index out of bound");
+    }
+    if (index === 0) {
+      return this.prepend(value);
+    } else if (this.length === index) {
+      return this.append(value);
+    }
+
+    // new node
+    const newNode = new Node(value);
+
+    //* leading node
+    const leadingNode = this.traverseToIndex(index - 1);
+    //* holding note
+    const holdingNode = leadingNode.next;
+
+    leadingNode.next = newNode;
+
+    newNode.next = holdingNode;
+    // console.log("Leading node ", leadingNode);
+    // console.log("Holding node ", holdingNode);
+
+    this.length++;
+    
+  }
+
+  remove(index) {
+    console.log(this.range);
+    if (index < 0 || index >= this.length) {
+      throw new Error("Index out of bound");
+    }
+    //remove from first
+    if (index === 0) {
+      const toBeRemove = this.head;
+      this.head = toBeRemove.next;
+      console.log(this.length);
+      this.length--;
+      console.log(this.length);
+
+      if (this.length === 0) {
+        this.tail = null;
+      }
+
+      return toBeRemove.data;
+    }
+
+    // remove from middle and last
+    const leadingNode = this.traverseToIndex(index - 1);
+    const toBeRemove = leadingNode.next;
+    leadingNode.next = toBeRemove.next;
+
+    if (leadingNode.next === null) {
+      this.tail = leadingNode;
+    }
+
+    this.length--;
+    return toBeRemove.data;
+  }
+  traverseToIndex(index) {
+    let currentNode = this.head;
+    let count = 0;
+    while (count !== index) {
+      currentNode = currentNode.next;
+      count++;
+    }
+
+    return currentNode;
+  }
+
+  print() {
+    // console.log(this.length)
+    // console.log(this.head);
+    let temp = this.head;
+    console.log(temp);
+    console.log(this.length);
+    const arr = [];
+    while (temp !== null) {
+      arr.push(temp.data);
+      temp = temp.next;
+    }
+
+    return arr;
+  }
+}
+
+const linkedList = new LinkedList();
+
+linkedList.append(1);
+linkedList.append(2);
+linkedList.append(3);
+linkedList.append(4);
+
+console.log(linkedList.print());
+console.log(linkedList.traverseToIndex(1));
+console.log(linkedList.insert(0, 0));
+console.log(linkedList.insert(2, 2));
+console.log(linkedList.insert(5, 5));
+console.log(linkedList.insert(6, 6));
+
+console.log(linkedList.print());
+console.log(linkedList.remove(3));
+console.log(linkedList.print());
